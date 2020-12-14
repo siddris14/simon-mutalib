ee32602fb002a801dd8138f633571bcd7615acee
Shaban Iddrisu™ <63567181+siddris14@users.noreply.github.com>
2020-12-14 21:37:22 +0400

    Update new-blog-details-page.js

--- new-animations/new-blog-details-page.js
+++ new-animations/new-blog-details-page.js
@@ -122,7 +122,6 @@ function initBlogDetailsPage() {
 	setTimeout(function () {
 		titleReveal();
 		heroImageReveal();
-		//heroParaReveal();
 		introImageOne();
 		introImageTwo();
 		blogImageReveal();
