cd5197c048cb590abfbe26705851176208761ead
Shaban Iddrisu™ <63567181+siddris14@users.noreply.github.com>
2020-12-19 14:48:19 +0400

    Update new-work-details.js

--- new-animations/new-work-details.js
+++ new-animations/new-work-details.js
@@ -33,8 +33,6 @@ function titleReveal() {
 
 	tl.from(title, { y: 233, stagger: 0.13 });
 	tl.from(briefSplit.lines, { y: 89, stagger: 0.13 }, "<");
-
-	return tl;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -49,8 +47,84 @@ function heroImageReveal() {
 
 	tl.from(image, { xPercent: 110, scale: 1.3, opacity: 0 });
 	tl.from(imageMask, { xPercent: -101 }, "<");
+}
 
-	return tl;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Summary Reveal Aniamtions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function summaryReveal() {
+	const summary = select(".work-temp-title-text");
+
+	const summarySplit = new SplitText(summary, { type: "chars lines words", linesClass: "summaryChild" });
+	new SplitText(summary, { type: "chars lines words", linesClass: "summaryParent" });
+
+	const tl = gsap.timeline({ id: "tl", defaults: { duration: 1.34, ease: "myEaseSmooth" } });
+
+	tl.from(summarySplit.lines, { y: 233, stagger: 0.13 });
+
+	ScrollTrigger.create({ trigger: ".temp-summary", start: "top 95%", animation: tl });
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Challenge & Solution Reveal Aniamtions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function challengeSolution() {
+	const challenges = selectAll(".temp-app-details-text");
+
+	challenges.forEach((line) => {
+		const lineSplit = new SplitText(line, { type: "chars lines words", linesClass: "lineChild" });
+		new SplitText(line, { type: "chars lines words", linesClass: "lineParent" });
+
+		const tl = gsap.timeline({ id: "tl", defaults: { duration: 1.34, ease: "myEaseSmooth" } });
+
+		tl.from(lineSplit.lines, { y: 233, stagger: 0.13 });
+
+		ScrollTrigger.create({ trigger: line, start: "top 95%", animation: tl });
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// View CTA Hover Aniamtions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function viewProjectCtaHover() {
+	const cta = select(".temp-view-cta-mask");
+	const ctaText = select(".temp-view-cta-text");
+	const bgPurple = selectAll(".temp-view-cta");
+
+	gsap.set(bgPurple, { yPercent: 101 });
+
+	cta.addEventListener("mouseenter", () => {
+		// cta in
+		gsap.to(cta, { scale: 1.2, duration: 0.55, ease: "myEaseSmooth", delay: 0 });
+		gsap.to(ctaText, { scale: 0.8, duration: 0.55, ease: "myEaseSmooth", delay: 0 });
+		gsap.to(bgPurple, { yPercent: 0, duration: 0.55, stagger: 0.05, ease: "myEaseSmooth", delay: 0 });
+	});
+
+	cta.addEventListener("mouseleave", () => {
+		// cta out
+		gsap.to(cta, { scale: 1, duration: 0.55, ease: "myEaseSmooth" });
+		gsap.to(ctaText, { scale: 1, duration: 0.55, ease: "myEaseSmooth", delay: 0 });
+		gsap.to(bgPurple, { yPercent: 101, duration: 0.55, stagger: 0.05, ease: "myEaseSmooth", delay: 0 });
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Project Image Reveal Aniamtions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function projectImagesReveal() {
+	const images = selectAll(".works-temp-images");
+
+	images.forEach((image) => {
+		const tl = gsap.timeline({ id: "tl", defaults: { duration: 1.34, ease: "myEaseSmooth" } });
+
+		tl.from(image, { opacity: 0, y: 233 });
+
+		ScrollTrigger.create({ trigger: image, start: "top 95%", animation: tl });
+	});
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -61,5 +135,9 @@ function initWorkDetailsPageAnimation() {
 	setTimeout(function () {
 		titleReveal();
 		heroImageReveal();
+		summaryReveal();
+		challengeSolution();
+		viewProjectCtaHover();
+		projectImagesReveal();
 	}, 1200);
 }
